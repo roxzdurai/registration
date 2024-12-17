c7a15837c5f891967964719063f3eb09114b8b58
RAJADURAI S <130030355+roxzdurai@users.noreply.github.com>
2024-12-17 09:02:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,10 +14,10 @@ app.use(bodyParser.json());
 
 // Database connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "2564", // Replace with your MySQL password
-    database: "employee_management",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
